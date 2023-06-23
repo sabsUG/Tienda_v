5be6c73eba371ab62d64ba1597aa77d013f060f3
sabsUG <107018722+sabsUG@users.noreply.github.com>
2023-06-23 16:47:27 -0600

    Firebase CRUD categoria
    Error with upload image and firebase

--- src/main/java/com/tienda/dao/CategoriaDao.java
+++ src/main/java/com/tienda/dao/CategoriaDao.java
@@ -2,7 +2,7 @@ package com.tienda.dao;
 
 import com.tienda.domain.Categoria;
 import org.springframework.data.jpa.repository.JpaRepository;
-
+//to use JPA, findAll, findByID...
 public interface CategoriaDao extends JpaRepository <Categoria,Long> {
     
 }
--- src/main/java/com/tienda/service/CategoriaService.java
+++ src/main/java/com/tienda/service/CategoriaService.java
@@ -3,9 +3,16 @@ package com.tienda.service;
 import com.tienda.domain.Categoria;
 import java.util.List;
 
+//methods to use 
 public interface CategoriaService {
     
     // Se obtiene un listado de categorias en un List
     public List<Categoria> getCategorias(boolean activos);
+    
+    public Categoria getCategoria(Categoria categoria);
+    
+    public void save(Categoria categoria);
+    
+    public void delete(Categoria categoria);
  
 }
